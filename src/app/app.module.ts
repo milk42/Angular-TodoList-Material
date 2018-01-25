787cb6b8a6f95deef889fc08c141d6dd0d26c8e6
Laetitia FOUCHERE <laetitia.fouchere.lp2@gmail.com>
2018-01-25 15:06:57 +0100

    Install Material + Menu Hamburger

---
+++ src/app/app.module.ts
@@ -0,0 +1,40 @@
+import { BrowserModule } from '@angular/platform-browser';
+import { NgModule } from '@angular/core';
+
+
+import { AppComponent } from './app.component';
+import { BodyComponent } from './body/body.component';
+import { HeaderComponent } from './header/header.component';
+import { FooterComponent } from './footer/footer.component';
+import { AppService } from './app.service';
+import { FlexLayoutModule } from '@angular/flex-layout';
+import { AddComponent } from './add/add.component';
+import { ListeComponent } from './liste/liste.component';
+import { TodoService } from './todo.service';
+import {BrowserAnimationsModule} from '@angular/platform-browser/animations';
+import {MatButtonModule, MatCheckboxModule} from '@angular/material';
+import {MatMenuModule} from '@angular/material/menu';
+import {MatIconModule} from '@angular/material/icon';
+
+@NgModule({
+  declarations: [  // ajouter les composants
+    AppComponent,
+    BodyComponent,
+    HeaderComponent,
+    FooterComponent,
+    AddComponent,
+    ListeComponent
+  ],
+  imports: [                     // ajouter les modules
+    BrowserModule,
+    FlexLayoutModule,
+    BrowserAnimationsModule,
+    MatButtonModule,
+    MatCheckboxModule,
+    MatMenuModule,
+    MatIconModule
+  ],
+  providers: [AppService, TodoService], // ajouter les services
+  bootstrap: [AppComponent]
+})
+export class AppModule { }
